ac9a75d7a85d67fa87979591b66eb0c274b3265a
Diogo Botas <30009700@students.ual.pt>
2022-04-26 10:56:21 +0100

    Lab02 DiogoBotas

---
+++ MVC/controller.py
@@ -0,0 +1,49 @@
+from models.LinkedList import LinkedList
+
+def inserir_inicio(lista_ligada, elemento):
+    lista_ligada.insert_at_start(elemento)
+    return lista_ligada
+
+def inserir_final(lista_ligada, elemento):
+    lista_ligada.insert_at_end(elemento)
+    return lista_ligada
+
+def inserir_apos_pais_registado(lista_ligada, registado, elemento):
+    lista_ligada.insert_after_item(registado, elemento)
+    return lista_ligada
+
+def inserir_antes_pais_registado(lista_ligada, registado, elemento):
+    lista_ligada.insert_before_item(registado, elemento)
+    return lista_ligada
+
+def inserir_num_determinado_indice(lista_ligada, valor, elemento):
+    lista_ligada.insert_at_index(elemento, int(valor))
+    return lista_ligada
+
+def verificar_numero_elementos(lista_ligada):
+    lista_ligada.get_count()
+    return lista_ligada
+
+def verificar_pais_lista(lista_ligada, elemento):
+    if lista_ligada.search_item(elemento) == True:
+        return True, lista_ligada
+    else:
+        return False
+
+def eliminar_primeiro_elemento(lista_ligada):
+    lista_ligada.delete_at_start()
+    return lista_ligada
+
+
+def eliminar_ultimo_elemento(lista_ligada):
+    lista_ligada.delete_at_end()
+    return lista_ligada
+
+
+def eliminar_pais_selecionado(lista_ligada, elemento):
+    if lista_ligada.search_item(elemento):
+        lista_ligada.delete_element_by_value(elemento)
+        return lista_ligada
+
+    else:
+        return False
---
+++ MVC/view.py
@@ -0,0 +1,66 @@
+from models.LinkedList import LinkedList
+import controller
+
+def main ():
+    lista_ligada = LinkedList()
+    while True:
+        comandos = input().split(" ")
+
+        if comandos [0] == "RPI":
+            if controller.inserir_inicio(lista_ligada, comandos[1]):
+                lista_ligada.traverse_list()
+
+
+        if comandos[0] == "RPF":
+            if controller.inserir_final(lista_ligada, comandos[1]):
+                lista_ligada.traverse_list()
+
+
+        if comandos[0] == "RPDE":
+            if controller.inserir_apos_pais_registado(lista_ligada, comandos[1], comandos[2]):
+                lista_ligada.traverse_list()
+
+
+        if comandos[0] == "RPAE":
+            if controller.inserir_antes_pais_registado(lista_ligada, comandos[1], comandos[2]):
+                lista_ligada.traverse_list()
+            
+
+        if comandos[0] == "RPII":
+            if controller.inserir_num_determinado_indice(lista_ligada, int(comandos[2]), comandos[1]):
+                lista_ligada.traverse_list()
+
+
+        if comandos[0] == "VNE":
+            if controller.verificar_numero_elementos(lista_ligada):
+                print(f"O número de elementos são {lista_ligada.get_count()}.")
+
+
+        if comandos[0] == "VP":
+            if controller.verificar_pais_lista(lista_ligada, comandos[1]):
+                print(f"O país {comandos[1]} encontra-se na lista.")
+            else:
+                print(f"O país {comandos[1]} não se encontra na lista.")
+
+
+        if comandos[0] == "EPE":
+            x = lista_ligada.start_node.item
+            if controller.eliminar_primeiro_elemento(lista_ligada):
+                print(f"O país {x} foi eliminado da lista.")
+
+
+        if comandos[0] == "EUE":
+            x = lista_ligada.get_last_node()
+            if controller.eliminar_ultimo_elemento(lista_ligada):
+                print(f"O país {x} foi eliminado da lista.")
+        
+
+
+        if comandos[0] == "EP":
+            if controller.verificar_pais_lista(lista_ligada, comandos[1]):
+                controller.eliminar_pais_selecionado(lista_ligada, comandos[1])
+                print(f"O país {comandos[1]} foi eliminado da lista.")
+            else:
+                print(f"O país {comandos[1]} não se encontra na lista.")
+
+
